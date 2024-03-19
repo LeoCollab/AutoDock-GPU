8c90b9c671ef0f547fe8f833a41e6767a506e019
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2024-03-19 09:16:22 -0500

    Adding printing code. So far corresponding printed values are correct.

--- dpcpp/kernels.dp.cpp
+++ dpcpp/kernels.dp.cpp
@@ -205,6 +205,7 @@ void reduce_via_matrix_units(sycl::nd_item<3> item, sycl::half *data_to_be_reduc
 
                 fill_Q(item, Q_data);
 
+                /*
                 if (groupId == 0 && localId == 0) {
                         printf("\nQ_data");
                         for (uint i = 0; i < 16 * 16; i++) {
@@ -213,6 +214,7 @@ void reduce_via_matrix_units(sycl::nd_item<3> item, sycl::half *data_to_be_reduc
                         }
                         printf("\n");
                 }
+                */
 
                 // Declaring and filling submatrices
                 joint_matrix<sycl::sub_group, sycl::half, use::b, rowscols_K, rowscols_N, layout::col_major> sub_P;
@@ -225,6 +227,32 @@ void reduce_via_matrix_units(sycl::nd_item<3> item, sycl::half *data_to_be_reduc
                 joint_matrix_fill(sg, sub_P, HALF_ONE); // P: only ones
                 joint_matrix_fill(sg, sub_V, HALF_ZERO); // Output: initialize to zeros
                 joint_matrix_fill(sg, sub_C, HALF_ZERO); // Final result
+
+                /*
+                // Must be copied from an accumulator matrix operand type
+                joint_matrix_store(sg, sub_V, sycl::local_ptr<sycl::half>(data_to_be_reduced), 16, layout::col_major);
+                if (groupId == 0 && localId == 0) {
+                        printf("\nsub_V");
+                        for (uint i = 0; i < 16 * 16; i++) {
+                                if ((i % 16) == 0) {printf("\n[Row %u]: ", i/16);}
+                                printf(" %5.3f ", float(data_to_be_reduced[i]));
+                        }
+                        printf("\n");
+                }
+                */
+
+                /*
+                // Must be copied from an accumulator matrix operand type
+                joint_matrix_store(sg, sub_C, sycl::local_ptr<sycl::half>(data_to_be_reduced), 16, layout::col_major);
+                if (groupId == 0 && localId == 0) {
+                        printf("\nsub_C");
+                        for (uint i = 0; i < 16 * 16; i++) {
+                                if ((i % 16) == 0) {printf("\n[Row %u]: ", i/16);}
+                                printf(" %5.3f ", float(data_to_be_reduced[i]));
+                        }
+                        printf("\n");
+                }
+                */
 
                 // TODO: check the entire data to processed will fit into the matrix registers
                 joint_matrix_load(sg, sub_Q, sycl::local_ptr<sycl::half>(Q_data), 16);
