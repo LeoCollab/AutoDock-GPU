33ea63f54e650c478740ba86bb32aea95f88509d
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2024-05-28 20:21:18 -0500

    Adding tabs to kernels.dp.cpp.

--- dpcpp/kernels.dp.cpp
+++ dpcpp/kernels.dp.cpp
@@ -35,39 +35,39 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 inline uint64_t llitoulli(int64_t l)
 {
 	uint64_t u;
-        /*
-        DPCT1053:0: Migration of device assembly code is not supported.
-        */
-        // asm("mov.b64    %0, %1;" : "=l"(u) : "l"(l));
-        u = l;
-        return u;
+	/*
+	DPCT1053:0: Migration of device assembly code is not supported.
+	*/
+	// asm("mov.b64    %0, %1;" : "=l"(u) : "l"(l));
+	u = l;
+	return u;
 }
 
 inline int64_t ullitolli(uint64_t u)
 {
 	int64_t l;
-        /*
-        DPCT1053:1: Migration of device assembly code is not supported.
-        */
-        // asm("mov.b64    %0, %1;" : "=l"(l) : "l"(u));
-        l = u;
-        return l;
+	/*
+	DPCT1053:1: Migration of device assembly code is not supported.
+	*/
+	// asm("mov.b64    %0, %1;" : "=l"(l) : "l"(u));
+	l = u;
+	return l;
 }
 
 /*
 DPCT1023:57: The DPC++ sub-group does not support mask options for shuffle.
 */
-#define WARPMINIMUMEXCHANGE(tgx, v0, k0, mask)                                 \
-        {                                                                      \
-                float v1 = v0;                                                               \
-                int k1 = k0;                                                                 \
-                int otgx = tgx ^ mask;                                                       \
-                float v2 = item_ct1.get_sub_group().shuffle(v0, otgx);                   \
-                int k2 = item_ct1.get_sub_group().shuffle(k0, otgx);                     \
-                int flag = ((v1 < v2) ^ (tgx > otgx)) && (v1 != v2);                         \
-                k0 = flag ? k1 : k2;                                                         \
-                v0 = flag ? v1 : v2;                                                         \
-        }
+#define WARPMINIMUMEXCHANGE(tgx, v0, k0, mask)	\
+{	\
+	float v1 = v0;	\
+	int k1 = k0;	\
+	int otgx = tgx ^ mask;	\
+	float v2 = item_ct1.get_sub_group().shuffle(v0, otgx);	\
+	int k2 = item_ct1.get_sub_group().shuffle(k0, otgx);	\
+	int flag = ((v1 < v2) ^ (tgx > otgx)) && (v1 != v2);	\
+	k0 = flag ? k1 : k2;	\
+	v0 = flag ? v1 : v2;	\
+}
 
 #define WARPMINIMUM2(tgx, v0, k0) \
 	WARPMINIMUMEXCHANGE(tgx, v0, k0, 1) \
@@ -80,92 +80,104 @@ DPCT1023:57: The DPC++ sub-group does not support mask options for shuffle.
 DPCT1023:40: The DPC++ sub-group does not support mask options for
 sycl::ext::oneapi::any_of.
 */
+
 /*
 DPCT1023:41: The DPC++ sub-group does not support mask options for shuffle.
 */
 
-#define REDUCEINTEGERSUM(value, pAccumulator)                                          \
-        int val = sycl::reduce_over_group(item_ct1.get_group(), value, std::plus<>()); \
-        *pAccumulator = val;                                                           \
-        item_ct1.barrier(SYCL_MEMORY_SPACE);
+#define REDUCEINTEGERSUM(value, pAccumulator)	\
+	int val = sycl::reduce_over_group(item_ct1.get_group(), value, std::plus<>());	\
+	*pAccumulator = val;	\
+	item_ct1.barrier(SYCL_MEMORY_SPACE);
 
 #define ATOMICADDI32(pAccumulator, value) \
-        sycl::atomic_ref<int, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) += ((int)(value))
+	sycl::atomic_ref<int, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) += ((int)(value))
 
 #define ATOMICSUBI32(pAccumulator, value) \
-        sycl::atomic_ref<int, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) -= ((int)(value))
+	sycl::atomic_ref<int, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) -= ((int)(value))
 
 #define ATOMICADDF32(pAccumulator, value) \
-		sycl::atomic_ref<float, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) += ((float)(value))
+	sycl::atomic_ref<float, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) += ((float)(value))
 
 #define ATOMICSUBF32(pAccumulator, value) \
-		sycl::atomic_ref<float, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) -= ((float)(value))
+	sycl::atomic_ref<float, SYCL_ATOMICS_MEMORY_ORDER, SYCL_ATOMICS_MEM_SCOPE, sycl::access::address_space::local_space>(*pAccumulator) -= ((float)(value))
 
 /*
 DPCT1023:11: The DPC++ sub-group does not support mask options for
 sycl::ext::oneapi::any_of.
 */
+
 /*
 DPCT1023:12: The DPC++ sub-group does not support mask options for shuffle.
 */
+
 /*
 DPCT1064:23: Migrated __any_sync call is used in a macro definition and is not
 valid for all macro uses. Adjust the code.
 */
+
 /*
 DPCT1064:24: Migrated __shfl_sync call is used in a macro definition and is not
 valid for all macro uses. Adjust the code.
 */
 
-#define REDUCEFLOATSUM(value, pAccumulator) \
-        value = sycl::reduce_over_group(item_ct1.get_group(), value, std::plus<>());\
-        *pAccumulator = (float) value;\
-        item_ct1.barrier(SYCL_MEMORY_SPACE);
+#define REDUCEFLOATSUM(value, pAccumulator)	\
+	value = sycl::reduce_over_group(item_ct1.get_group(), value, std::plus<>());	\
+	*pAccumulator = (float) value;	\
+	item_ct1.barrier(SYCL_MEMORY_SPACE);
 
 static dpct::constant_memory<GpuData, 0> cData;
 static GpuData cpuData;
 
-void SetKernelsGpuData(GpuData *pData) try {
-        int status;
-        /*
-        DPCT1003:4: Migrated API does not return error code. (*, 0) is inserted.
-        You may need to rewrite this code.
-        */
-        status = (dpct::get_default_queue()
-                      .memcpy(cData.get_ptr(), pData, sizeof(GpuData))
-                      .wait(),
-                  0);
-        /*
-        DPCT1001:2: The statement could not be removed.
-        */
-        RTERROR(status, "SetKernelsGpuData copy to cData failed");
-        memcpy(&cpuData, pData, sizeof(GpuData));
+void SetKernelsGpuData(GpuData *pData) try
+{
+	int status;
+
+	/*
+	DPCT1003:4: Migrated API does not return error code. (*, 0) is inserted.
+	You may need to rewrite this code.
+	*/
+	status = (dpct::get_default_queue()
+				.memcpy(cData.get_ptr(), pData, sizeof(GpuData))
+				.wait(),
+				0);
+
+	/*
+	DPCT1001:2: The statement could not be removed.
+	*/
+	RTERROR(status, "SetKernelsGpuData copy to cData failed");
+	memcpy(&cpuData, pData, sizeof(GpuData));
 }
-catch (sycl::exception const &exc) {
-  std::cerr << exc.what() << "Exception caught at file:" << __FILE__
-            << ", line:" << __LINE__ << std::endl;
-  std::exit(1);
+catch (sycl::exception const &exc)
+{
+	std::cerr << exc.what() << "Exception caught at file:" << __FILE__
+			  << ", line:" << __LINE__ << std::endl;
+	std::exit(1);
 }
 
-void GetKernelsGpuData(GpuData *pData) try {
-        int status;
-        /*
-        DPCT1003:7: Migrated API does not return error code. (*, 0) is inserted.
-        You may need to rewrite this code.
-        */
-        status = (dpct::get_default_queue()
-                      .memcpy(pData, cData.get_ptr(), sizeof(GpuData))
-                      .wait(),
-                  0);
-        /*
-        DPCT1001:6: The statement could not be removed.
-        */
-        RTERROR(status, "GetKernelsGpuData copy From cData failed");
+void GetKernelsGpuData(GpuData *pData) try
+{
+	int status;
+
+	/*
+	DPCT1003:7: Migrated API does not return error code. (*, 0) is inserted.
+	You may need to rewrite this code.
+	*/
+	status = (dpct::get_default_queue()
+				.memcpy(pData, cData.get_ptr(), sizeof(GpuData))
+				.wait(),
+				0);
+
+	/*
+	DPCT1001:6: The statement could not be removed.
+	*/
+	RTERROR(status, "GetKernelsGpuData copy From cData failed");
 }
-catch (sycl::exception const &exc) {
-  std::cerr << exc.what() << "Exception caught at file:" << __FILE__
-            << ", line:" << __LINE__ << std::endl;
-  std::exit(1);
+catch (sycl::exception const &exc)
+{
+	std::cerr << exc.what() << "Exception caught at file:" << __FILE__
+			  << ", line:" << __LINE__ << std::endl;
+	std::exit(1);
 }
 
 // Kernel files
