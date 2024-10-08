0e491a58905066dd921e56c6e6b73d3b6191ecc0
Leonardo Solis-Vasquez (from HRZ) <leonardosolis86@gmail.com>
2024-10-08 21:53:51 +0200

    Updating SLURM script.

--- batch_hrz_sycl.sh
+++ batch_hrz_sycl.sh
@@ -13,8 +13,8 @@
 module load intel/2024.1
 
 # Executing the SYCL version
-WORK_DIR=/home/ae89olan/project02441/ad-gpu-sycl-clean
-cd ${WORK_DIR} && pwd && make DEVICE=XeGPU TESTLS=sw test && make DEVICE=XeGPU TESTLS=ad test
+WORK_DIR=${PWD}
+cd ${WORK_DIR} && echo "${WORK_DIR}" && make DEVICE=XeGPU TESTLS=sw test && make DEVICE=XeGPU TESTLS=ad test
 
 exit 0
 
